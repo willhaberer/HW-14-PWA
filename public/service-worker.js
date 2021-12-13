6cbe1f1c155aa26df4d2f16bd910dc143293c101
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-13 17:02:03 -0500

    adde cache strat

--- public/service-worker.js
+++ public/service-worker.js
@@ -38,3 +38,38 @@ self.addEventListener("activate", function (evt) {
 
   self.clients.claim();
 });
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
