73fe4d8d903a0407cc107699430c615c698ede21
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-13 16:59:27 -0500

    added event listener to the service worker for install

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,14 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
